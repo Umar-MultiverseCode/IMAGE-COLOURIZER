8472ffbcf610d77bdfef0e106b812826afe11901
Umar Mulla <139649458+Umar-MultiverseCode@users.noreply.github.com>
2025-01-19 00:38:18 +0530

    Up-to-date

--- colorize.py
+++ colorize.py
@@ -5,7 +5,7 @@ import cv2
 import os
 
 """
-Download the model files: 
+model files: 
 	1. colorization_deploy_v2.prototxt:    https://github.com/richzhang/colorization/tree/caffe/colorization/models 
 	2. pts_in_hull.npy:					   https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
 	3. colorization_release_v2.caffemodel: https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1 
